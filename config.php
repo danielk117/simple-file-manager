1091052f7a91ca74a71bde21bd9456d32f3cf5d5
danielk117 <36566093+danielk117@users.noreply.github.com>
2022-02-25 14:14:26 +0100

    edit is configureable

--- config.php
+++ config.php
@@ -7,11 +7,12 @@
 
 $base_dir = ''; // set base directory, something like '/.git'
 
-$allow_delete = true; // set to false to disable delete button and delete POST request
-$allow_upload = true; // set to true to allow upload files
-$allow_create_folder = true; // set to false to disable folder creation
-$allow_direct_link = true; // set to false to only allow downloads and not direct link
-$allow_show_folders = true; // set to false to hide all subdirectories
+$allow_delete = true; // set false to disable delete button and delete POST request
+$allow_upload = true; // set true to allow upload files
+$allow_edit = true; // set true to allow edit files
+$allow_create_folder = true; // set false to disable folder creation
+$allow_direct_link = true; // set false to only allow downloads and not direct link
+$allow_show_folders = true; // set false to hide all subdirectories
 
 $disallowed_patterns = ['*.php'];  // matching files not allowed to be uploaded
 $hidden_patterns = ['*.php','.*']; // matching files hidden in directory index
